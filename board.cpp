4b74b5ac51fbb62f586e08720b1f6ec78bfde0a0
eric-moreno <emoreno@caltech.edu>
2018-03-04 22:15:44 -0800

    tests with heuristics

--- board.cpp
+++ board.cpp
@@ -240,7 +240,7 @@ int Board::weightMove(Move *move, Side side) {
     int pieces_val = temp->count(side) - temp->count(OPPONENT_SIDE);
 
     int mobility =  temp->getMoves(side).size();
-    return  3 * mobility + 2 * static_val + pieces_val;
+    return  mobility + 3 * static_val + pieces_val;
 }
     
 /*
